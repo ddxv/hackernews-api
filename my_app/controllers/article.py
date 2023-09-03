a438f70d8ba7a45998ef935dc215bd72b3ebb534
james <james@bubbleye.com>
2023-09-03 21:01:45 +0800

    added docstring for function

--- my_app/controllers/article.py
+++ my_app/controllers/article.py
@@ -15,6 +15,15 @@ class ArticleController(Controller):
 
     @get(path="/{article_id:int}")
     async def get_article(self, article_id: int) -> Article:
+        """
+        Handles a GET request for a specific article.
+
+        Args:
+            article_id (int): The id of the article to retrieve.
+
+        Returns:
+            Article: A dictionary representation of the article.
+        """
         print(f"GET for {article_id=}")
         df = query_article(article_id)
         mydict: Article = df.set_index("id").to_dict(orient="index")
@@ -24,6 +33,17 @@ class ArticleController(Controller):
     async def get_article_list(
         self, type: str, page: int = 1, items_per_page: int = 50
     ) -> Articles:
+        """
+        Handles a GET request for a list of articles of a certain type.
+
+        Args:
+            type (str): The type of articles to retrieve.
+            page (int, optional): The page number of the articles list. Defaults to 1.
+            items_per_page (int, optional): The number of articles per page. Defaults to 50.
+
+        Returns:
+            Articles: A dictionary representation of the list of articles.
+        """
         print(f"GET for {type=} and {page=}")
         if type in ["top", "new", "best"]:
             df = query_type(type=type, page_number=page, items_per_page=items_per_page)
